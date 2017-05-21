14b4a701369c6ba8ecb22999dc0934faf850a9a7
Andras <andras@kinvey.com>
2017-05-21 01:42:12 -0400

    test / coverage / clean scripts, more tests

--- test-timeit.js
+++ test-timeit.js
@@ -1,10 +1,19 @@
 var timeit = require('./');
 
+var SILENT = '/* NOOUTPUT */';          // suppress test output
+
 module.exports = {
-    'should export timeit, reportit and fptime': function(t) {
+    'should export timeit, reportit, fptime and bench': function(t) {
         t.equal('function', typeof timeit);
         t.equal('function', typeof timeit.reportit);
         t.equal('function', typeof timeit.fptime);
+        t.equal('function', typeof timeit.bench);
+        t.done();
+    },
+
+    'should export sysinfo, cpuMhz': function(t) {
+        t.equal('function', typeof timeit.sysinfo);
+        t.equal('function', typeof timeit.cpuMhz);
         t.done();
     },
 
@@ -15,19 +24,44 @@ module.exports = {
         t.done();
     },
 
-    'timeit should time function': function(t) {
-        var ncalls = 0;
-        timeit(1000000, function(){ ncalls += 1 }, '/* NOOUTPUT */');
-        t.ok(ncalls >= 1000000);
-        t.done();
-    },
-
-    'timeit should time functions with callback': function(t) {
-        t.expect(1);
-        var ncalls = 0;
-        timeit(1000000, function(cb){ ncalls += 1; cb(); }, '/* NOOUTPUT */', function(){
+    'timeit()': {
+        'timeit should time counted function': function(t) {
+            var ncalls = 0;
+            timeit(1000000, function(){ ncalls += 1 }, SILENT);
             t.ok(ncalls >= 1000000);
             t.done();
-        });
-    }
+        },
+
+        'timeit should time timed function': function(t) {
+            var t1 = Date.now();
+            var ncalls = 0;
+            timeit(0.030, function(){ ncalls += 1 }, SILENT);
+            t.ok(Date.now() - t1 >= 30);
+            t.done();
+        },
+    },
+
+    'timeit(cb)': {
+        'timeit should time counted functions with callback': function(t) {
+            t.expect(1);
+            var ncalls = 0;
+            timeit(1000000, function(cb){ ncalls += 1; cb(); }, SILENT, function(){
+                t.ok(ncalls >= 1000000);
+                t.done();
+            });
+        },
+
+        'timeit should time timed functions with callback': function(t) {
+            t.expect(1);
+            var t1 = Date.now();
+            var ncalls = 0;
+            timeit(0.030, function(cb){ ncalls += 1; cb(); }, SILENT, function(){
+                t.ok(Date.now() - t1 >= 30);
+                t.done();
+            });
+        },
+    },
+
+    'bench': {
+    },
 };
